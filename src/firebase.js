621a356a688eadaa48b8575ccf6a49a7632bf6be
PG <plamen5020@gmail.com>
2024-08-15 19:22:35 +0300

    added env file

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC-KvURdAdtYV08YZZqRV7NZrs5q6TMNbQ",
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: "anime-calendar-43329.firebaseapp.com",
   projectId: "anime-calendar-43329",
   storageBucket: "anime-calendar-43329.appspot.com",
-  messagingSenderId: "955240602889",
-  appId: "1:955240602889:web:440e7028f73ae5e58987d8",
+  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_API_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export default app;
